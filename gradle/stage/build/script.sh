8a4ef1b5ea5fcf09533339e159250946b860ab51
Alan Yeh <alan@yeh.cn>
2024-01-25 03:38:13 +0800

    [ADD] 新增 Gradle 镜像
    Signed-off-by: Alan Yeh <alan@yeh.cn>

---
+++ gradle/stage/build/script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 脚本执行失败时脚本终止执行
+set -o errexit
+# 遇到未声明变量时脚本终止执行
+set -o nounset
+# 执行管道命令时，只要有一个子命令失败，则脚本终止执行
+set -o pipefail
+# 打印执行过程
+set -o xtrace
+
+# 取参数
+target=""
+package=""
+
+for arg in "$@" ; do
+    case "$arg" in
+        --target=*)
+            target="${arg#*=}"
+        ;;
+        --package=*)
+            package="${arg#*=}"
+        ;;
+    esac
+done
+
+# 下载安装包
+wget -O ./gradle.zip "$package"
+
+# 解压
+mkdir ./cache
+unzip ./gradle.zip -d ./cache
+# 删除安装包，释放空间
+rm -f ./gradle.zip
+# 将安装包复制到指定路径
+mkdir -p "$target"
+mv ./cache/*/* "$target"
